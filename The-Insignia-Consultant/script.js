8b5eff6beaf0a7e0b211d2aab378c3d6a15cf53c
Swapnil Kuthe <swapnilkuthe20@gmail.com>
2025-07-16 19:21:02 +0530

    landing page done but responsive navbar remailning

---
+++ The-Insignia-Consultant/script.js
@@ -0,0 +1,8 @@
+// Step 1: Select hamburger and nav-links
+const hamburger = document.getElementById('hamburger-btn');
+const navLinks = document.getElementById('nav-links');
+
+// Step 2: Add click event
+hamburger.addEventListener('click', () => {
+    // navLinks.classList.toggle('active');
+});
