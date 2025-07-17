971008b3b4ba787f06c222dbbbde92d5b6ede9f0
Swapnil Kuthe <swapnilkuthe20@gmail.com>
2025-07-17 17:21:20 +0530

    Navbar responsive completed done

--- The-Insignia-Consultant/script.js
+++ The-Insignia-Consultant/script.js
@@ -1,8 +1,14 @@
+
 // Step 1: Select hamburger and nav-links
-const hamburger = document.getElementById('hamburger-btn');
+
+const hamburger = document.getElementById('hamburger-menu');
 const navLinks = document.getElementById('nav-links');
 
+console.log(hamburger, "...hamburger");
+
 // Step 2: Add click event
+
 hamburger.addEventListener('click', () => {
-    // navLinks.classList.toggle('active');
+    hamburger.classList.toggle('fa-xmark');
+    navLinks.classList.toggle('active');
 });
